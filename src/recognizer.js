4226ae3e9145c3d8a18046b6e4c41a0973cde774
dqisme <dqisme@outlook.com>
2016-07-22 00:34:16 +0800

    [dq] #N/A make recognizer can recognize string by its form

--- src/recognizer.js
+++ src/recognizer.js
@@ -1,5 +1,8 @@
 var Recognizer = function(form) {
   this.form = form;
+  this.recognize = function (string) {
+    return this.form.test(string);
+  }
 };
 
 module.exports = Recognizer;
--- test/recognizer.test.js
+++ test/recognizer.test.js
@@ -11,4 +11,17 @@ describe('test recognizer', function () {
     var recognizer = new Recognizer(form);
     expect(recognizer.form).to.be.equal(form);
   });
+
+  describe('recognize by form', function () {
+
+    it('should can be recognize a string by its form as a Regular Expression', function () {
+
+      var form = /foo/;
+      var string = 'foo';
+      var wrongString = 'bar';
+      var recognizer = new Recognizer(form);
+      expect(recognizer.recognize(string)).to.be.true;
+      expect(recognizer.recognize(wrongString)).to.be.not.true;
+    });
+  });
 });
