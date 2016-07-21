e225380ad07c50055d05d9199d7d40ae08af3e48
dqisme <dqisme@outlook.com>
2016-07-21 23:15:21 +0800

    [dq] #N/A add test for letter words identifier

--- test/church.test.js
+++ test/church.test.js
@@ -3,19 +3,24 @@ var expect = require('chai').expect;
 
 describe('test church', function () {
   describe('test identifiers recognition', function () {
+
     it('should can recognize integer as identifier', function () {
       expect(church.isIdentifier('1')).to.be.true;
       expect(church.isIdentifier('2')).to.be.true;
       expect(church.isIdentifier('10')).to.be.true;
     });
+
     it('should can recognize point number as not identifier', function () {
       expect(church.isIdentifier('10.1')).to.be.false;
     });
+
     it('should can recognize empty string as not identifier', function () {
       expect(church.isIdentifier('')).to.be.false;
     });
-    it('should can recognize empty string as not identifier', function () {
-      expect(church.isIdentifier('')).to.be.false;
+
+    it('should can recognize letter words as identifier', function () {
+      expect(church.isIdentifier('foo')).to.be.true;
+      expect(church.isIdentifier('bar')).to.be.true;
     });
   });
 });
