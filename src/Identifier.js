d5ffe0cdf55eadb732ea3f996c081f7a309f5de5
dqisme <dqisme@outlook.com>
2016-07-22 11:13:27 +0800

    [dq] #5 make identifier as a class

---
+++ src/Identifier.js
@@ -0,0 +1,5 @@
+var Identifier = function () {
+
+};
+
+module.exports = Identifier;
--- test/Identifier.test.js
+++ test/Identifier.test.js
@@ -1,1 +1,8 @@
 var Identifier = require('../src/Identifier');
+
+describe('Identifier', function () {
+
+  it('should be a function', function () {
+    expect(Identifier).to.be.a('function');
+  });
+});
