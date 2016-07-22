9bf431401e66c7294038aa7da4b012a92f60dd70
dqisme <dqisme@outlook.com>
2016-07-22 14:25:26 +0800

    [dq] #5 make identifier constructor for detect content by its form

--- src/Identifier.js
+++ src/Identifier.js
@@ -2,9 +2,18 @@ var LambdaTerm = require('./LambdaTerm');
 
 var Identifier = function (content) {
   this.content = content;
+  try {
+    if (!this.form.test(content)) {
+      throw new Error('content is NOT a identifier');
+    }
+  } catch (e) {
+    if(e) {
+      this.error = e;
+    }
+  }
 };
 
-var identifier_form = /\w+/;
+var identifier_form = /^[a-zA-Z]+$/;
 Identifier.prototype = new LambdaTerm(identifier_form);
 
 module.exports = Identifier;
--- test/Identifier.test.js
+++ test/Identifier.test.js
@@ -19,8 +19,36 @@ describe('Identifier', function () {
   });
 
   it('should have form as regular expression as \\w+', function () {
-    var identifier_form = /\w+/;
+    var identifierForm = /^[a-zA-Z]+$/;
     var identifier = new Identifier();
-    expect(identifier.form).to.be.eql(identifier_form);
+    expect(identifier.form).to.be.eql(identifierForm);
+  });
+
+  it('should can be constructed with content that are one letter', function () {
+    var letter = 'x';
+    var identifier = new Identifier(letter);
+    expect(identifier).to.not.have.property('error');
+  });
+
+  it('should can be constructed with content that are a word', function () {
+    var word = 'foo';
+    var identifier = new Identifier(word);
+    expect(identifier).to.not.have.property('error');
+  });
+
+  it('should have a error property' +
+    ' when constructed with content that are a digital', function () {
+
+    var digital = '1';
+    var identifier = new Identifier(digital);
+    expect(identifier).to.have.property('error');
+  });
+
+  it('should have a error property' +
+    ' when constructed with content that are a number', function () {
+
+    var number = '123';
+    var identifier = new Identifier(number);
+    expect(identifier).to.have.property('error');
   });
 });
