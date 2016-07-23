f763211e1c4098123759efa6cd3f79fb86deb466
dqisme <dqisme@outlook.com>
2016-07-23 21:44:42 +0800

    [dq] #N/A refactor: move variable's error detection to lambda term

--- src/LambdaTerm.js
+++ src/LambdaTerm.js
@@ -1,6 +1,15 @@
 var LambdaTerm = function (form, content) {
-  this.form = form;
+  this.form = form || /^.*$/;
   this.content = content;
+  try {
+    if (!this.form.test(content)) {
+      throw new Error('content is NOT a valid lambda term');
+    }
+  } catch (error) {
+    if(error) {
+      this.error = error;
+    }
+  }
 };
 
 module.exports = LambdaTerm;
--- src/Variable.js
+++ src/Variable.js
@@ -3,15 +3,6 @@ var LambdaTerm = require('./LambdaTerm');
 var Variable = function (content) {
   var form = /^[a-zA-Z]+$/;
   this.constructor(form, content);
-  try {
-    if (!this.form.test(content)) {
-      throw new Error('content is NOT a variable');
-    }
-  } catch (error) {
-    if(error) {
-      this.error = error;
-    }
-  }
 };
 
 Variable.prototype = new LambdaTerm();
