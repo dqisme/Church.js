ab306bb0d7df08f48c63449afb89832ff2b06715
dqisme <dqisme@outlook.com>
2016-07-25 00:09:42 +0800

    [dq] #N/A refactor to drop try catch for lambda term construction error

--- src/LambdaTerm.js
+++ src/LambdaTerm.js
@@ -1,16 +1,10 @@
 var LambdaTerm = function (form, content, mutator) {
   this.form = form || /^.*$/;
   this.content = content;
-  try {
-    if (!this.form.test(content)) {
-      throw new Error('content is NOT a valid lambda term');
-    } else if (typeof mutator === 'function') {
-      mutator(this.form.exec(content));
-    }
-  } catch (error) {
-    if(error) {
-      this.error = error;
-    }
+  if (!this.form.test(content)) {
+    this.error = 'content is NOT a valid lambda term';
+  } else if (typeof mutator === 'function') {
+    mutator(this.form.exec(content));
   }
 };
 
