4d0fbe3bdace4674463d29220ebfaf01bc08db4e
dqisme <dqisme@outlook.com>
2016-07-24 22:22:04 +0800

    [dq] #N/A refactor abstraction for constructor with lambda term

--- src/Abstraction.js
+++ src/Abstraction.js
@@ -2,20 +2,15 @@ var LambdaTerm = require('./LambdaTerm');
 var Variable = require('./Variable');
 
 var Abstraction = function (content) {
-  this.content = content;
-  try {
-    if (!this.form.test(content)) {
-      throw new Error('content is NOT a abstraction');
-    } else {
-      this.boundVariable = new Variable(this.form.exec(content)[1]);
-      this.body = this.form.exec(content)[2];
-    }
-  } catch (error) {
-    this.error = error;
-  }
+  var form = /^&(.+?)\.(.+?)$/;
+  var self = this;
+  var mutator = function (terms) {
+    self.boundVariable = new Variable(terms[1]);
+    self.body = terms[2];
+  };
+  this.constructor(form, content, mutator);
 };
 
-var abstractionForm = /^&(.+?)\.(.+?)$/;
-Abstraction.prototype = new LambdaTerm(abstractionForm);
+Abstraction.prototype = new LambdaTerm();
 
 module.exports = Abstraction;
--- src/LambdaTerm.js
+++ src/LambdaTerm.js
@@ -1,9 +1,11 @@
-var LambdaTerm = function (form, content) {
+var LambdaTerm = function (form, content, mutator) {
   this.form = form || /^.*$/;
   this.content = content;
   try {
     if (!this.form.test(content)) {
       throw new Error('content is NOT a valid lambda term');
+    } else if (typeof mutator === 'function') {
+      mutator(this.form.exec(content));
     }
   } catch (error) {
     if(error) {
