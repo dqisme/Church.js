b1c3b6eaac960ed4266ba1f18d689dfa81aa61fe
dqisme <dqisme@outlook.com>
2016-08-21 01:55:28 +0800

    [dq] #N/A make parentheses body can be constructed to a relevant lambda term

--- src/Parentheses.js
+++ src/Parentheses.js
@@ -4,7 +4,16 @@ var Parentheses = function (content) {
   var form = /^\((.+?)\)$/;
   var self = this;
   var mutator = function (terms) {
-    self.body = terms[1];
+    var Variable = require('./Variable');
+    var Abstraction = require('./Abstraction');
+    var Application = require('./Application');
+    [Variable, Abstraction, Application, Parentheses].some(function (LambdaTermType) {
+      var lambdaTerm = new LambdaTermType(terms[1]);
+      if (lambdaTerm.valid) {
+        self.body = lambdaTerm;
+        return true;
+      }
+    });
   };
   this.constructor(form, content, mutator);
 };
--- test/Parentheses.test.js
+++ test/Parentheses.test.js
@@ -1,4 +1,7 @@
 var Parentheses = require('../src/Parentheses');
+var Variable = require('../src/Variable');
+var Abstraction = require('../src/Abstraction');
+var Application = require('../src/Application');
 
 describe('Parentheses Convention', function () {
   it('should be a function', function () {
@@ -22,5 +25,41 @@ describe('Parentheses Convention', function () {
     var parentheticalContent = '(x)';
     var parentheses = new Parentheses(parentheticalContent);
     expect(parentheses).to.have.property('body');
+  });
+
+  it('should have a body which is a variable when constructed with a parentheses', function () {
+    var bodyContentAsVariable = 'x';
+    var parenthesesContent = '(' + bodyContentAsVariable + ')';
+    var parentheses = new Parentheses(parenthesesContent);
+    expect(parentheses.body).to.be.an.instanceOf(Variable);
+    var bodyAsVariable = new Variable(bodyContentAsVariable);
+    expect(parentheses.body).to.be.eql(bodyAsVariable);
+  });
+
+  it('should have a body which is a abstraction when constructed with a parentheses', function () {
+    var bodyContentAsAbstraction = '&x.y';
+    var parenthesesContent = '(' + bodyContentAsAbstraction + ')';
+    var parentheses = new Parentheses(parenthesesContent);
+    expect(parentheses.body).to.be.an.instanceOf(Abstraction);
+    var bodyAsVariable = new Abstraction(bodyContentAsAbstraction);
+    expect(parentheses.body).to.be.eql(bodyAsVariable);
+  });
+
+  it('should have a body which is a application when constructed with a parentheses', function () {
+    var bodyContentAsApplication = 'x y';
+    var parenthesesContent = '(' + bodyContentAsApplication + ')';
+    var parentheses = new Parentheses(parenthesesContent);
+    expect(parentheses.body).to.be.an.instanceOf(Application);
+    var bodyAsVariable = new Application(bodyContentAsApplication);
+    expect(parentheses.body).to.be.eql(bodyAsVariable);
+  });
+
+  it('should have a body which is parentheses when constructed with a parentheses', function () {
+    var bodyContentAsParentheses = '(x)';
+    var parenthesesContent = '(' + bodyContentAsParentheses + ')';
+    var parentheses = new Parentheses(parenthesesContent);
+    expect(parentheses.body).to.be.an.instanceOf(Parentheses);
+    var bodyAsVariable = new Parentheses(bodyContentAsParentheses);
+    expect(parentheses.body).to.be.eql(bodyAsVariable);
   });
 });
