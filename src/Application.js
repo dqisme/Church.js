e45e9baccff0f83b7f00043d416658a2a1187858
dqisme <dqisme@outlook.com>
2016-08-22 11:06:58 +0800

    [dq] #8 refactor: replace application function and argument constructing by lambda term

--- src/Application.js
+++ src/Application.js
@@ -5,22 +5,9 @@ var Application = function (content) {
   this.content = content;
   var self = this;
   var mutator = function (terms) {
-    var Variable = require('./Variable');
-    var Abstraction = require('./Abstraction');
-    var Parentheses = require('./Parentheses');
-    var schema = {
-      function: terms[1],
-      argument: terms[2]
-    };
-    Object.keys(schema).forEach(function (property) {
-      [Variable, Abstraction, Application, Parentheses].some(function (LambdaTermType) {
-        var lambdaTerm = new LambdaTermType(schema[property]);
-        if (lambdaTerm.valid) {
-          self[property] = lambdaTerm;
-          return true;
-        }
-      });
-    });
+    var LambdaTerm = require('./LambdaTerm');
+    self.function = new LambdaTerm(terms[1]);
+    self.argument = new LambdaTerm(terms[2]);
   };
   this.constructor(form, content, mutator);
 };
