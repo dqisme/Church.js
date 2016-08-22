85629261ecddacebd2591adfb660bf985ac161ab
dqisme <dqisme@outlook.com>
2016-08-22 11:08:13 +0800

    [dq] #8 refactor: replace parentheses body constructing by lambda term

--- src/Parentheses.js
+++ src/Parentheses.js
@@ -4,16 +4,8 @@ var Parentheses = function (content) {
   var form = /^\((.+?)\)$/;
   var self = this;
   var mutator = function (terms) {
-    var Variable = require('./Variable');
-    var Abstraction = require('./Abstraction');
-    var Application = require('./Application');
-    [Variable, Abstraction, Application, Parentheses].some(function (LambdaTermType) {
-      var lambdaTerm = new LambdaTermType(terms[1]);
-      if (lambdaTerm.valid) {
-        self.body = lambdaTerm;
-        return true;
-      }
-    });
+    var LambdaTerm = require('./LambdaTerm');
+    self.body = new LambdaTerm(terms[1]);
   };
   this.constructor(form, content, mutator);
 };
